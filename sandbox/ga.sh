fc191885031a1514511e02f5bc4ea40327fe4db6
alstonlo <40709307+alstonlo@users.noreply.github.com>
2024-05-02 04:04:53 -0400

    fix arg names

--- sandbox/ga.sh
+++ sandbox/ga.sh
@@ -1,12 +1,12 @@
 export PYTHONPATH="${HOME}/SynTreeNet/src"
 
 python sandbox/optimize_ga.py \
-    --skeleton-set-file /ssd/msun415/skeletons-top-1000-valid.pkl \
-    --ckpt-rxn /ssd/msun415/surrogate/version_38/ \
-    --ckpt-bb /ssd/msun415/surrogate/version_37/ \
-    --filter-only rxn bb \
-    --hash-dir /ssd/msun415/hash_table-bb=1000-prods=2_new/ \
-    --top-bbs-file /ssd/msun415/bblocks-top-1000.txt \
+    --skeleton_set_file /ssd/msun415/skeletons-top-1000-valid.pkl \
+    --ckpt_rxn /ssd/msun415/surrogate/version_38/ \
+    --ckpt_bb /ssd/msun415/surrogate/version_37/ \
+    --filter_only rxn bb \
+    --hash_dir /ssd/msun415/hash_table-bb=1000-prods=2_new/ \
+    --top_bbs_file /ssd/msun415/bblocks-top-1000.txt \
     --objective drd2 \
-    --out-dir $HOME/SynTreeNet/results/viz/top_1000 \
+    --out_dir $HOME/SynTreeNet/results/viz/top_1000 \
     --enable_wandb
