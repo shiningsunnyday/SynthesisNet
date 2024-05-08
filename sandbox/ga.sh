d6d58b9c04d41a1ef689fa5e76929da3be6de539
alstonlo <40709307+alstonlo@users.noreply.github.com>
2024-05-07 20:55:57 -0400

    Update ga.sh

--- sandbox/ga.sh
+++ sandbox/ga.sh
@@ -2,7 +2,7 @@ export PYTHONPATH="${HOME}/SynTreeNet/src"
 MAX_NUM_RXNS=4
 
 python sandbox/optimize.py \
-    --skeleton_set_file /ssd/msun415/skeletons-valid.pkl \
+    --skeleton_set_file /ssd/msun415/skeletons/skeletons-valid.pkl \
     --ckpt_rxn /ssd/msun415/surrogate/${MAX_NUM_RXNS}-RXN/ \
     --ckpt_bb /ssd/msun415/surrogate/${MAX_NUM_RXNS}-NN/ \
     --ckpt_recognizer /ssd/msun415/surrogate/${MAX_NUM_RXNS}-REC/ \
