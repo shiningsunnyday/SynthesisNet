a9631a12e140e5ff52e4a86913b050df26bf005b
alstonlo <alston@mit.edu>
2024-08-02 00:01:02 -0400

    change cmnd a bit

--- sandbox/ga_ablate_f12.sh
+++ sandbox/ga_ablate_f12.sh
@@ -17,15 +17,15 @@ python sandbox/optimize.py \
     --max_topological_orders 5 \
     --objective $1 \
     --wandb=true \
-    --wandb_project=syntreenet_ga_rebuttal_v3 \
+    --wandb_project=alston_syntreenet_ga_rebuttal_v3 \
     --method=ours \
-    --num_workers=50 \
+    --num_workers=30 \
     --fp_bits=2048 \
     --bt_mutate_edits=3 \
     --early_stop=true \
     --early_stop_delta=0.01 \
     --early_stop_warmup=30 \
-    --early_stop_patience=10 \
+    --early_stop_patience=10
     --fp_mutate_prob=0.5 \
     --children_strategy "topk" \
     --max_oracle_workers=0
